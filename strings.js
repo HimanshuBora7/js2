81733dfc5fbd43391a85260125413c47435f8983
Bochan <himanshubora972@gmail.com>
2025-06-10 21:29:51 +0530

    day1

---
+++ strings.js
@@ -0,0 +1,26 @@
+//console.log("hello world")
+//string are objets in js
+
+const gameName = new String("hello world");
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length)
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("e")+1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
