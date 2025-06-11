0b67670297bfad9b9dd2bf0450aabe0fcca97e9f
Bochan <himanshubora972@gmail.com>
2025-06-11 17:01:50 +0530

    strings_finish

--- strings.js
+++ strings.js
@@ -10,13 +10,21 @@ console.log(gameName.length)
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf("e")+1)
 
+//slicing the string 
+const gameName1 = new String("Bochan-kasukabe")
 
+const sliced_string = gameName1.substring(0,6);
+console.log(sliced_string);
 
+const newStringOne = "           bochie     "
 
+console.log(newStringOne)
+console.log(newStringOne.trim())
 
+const url ="https://github.com/himanshubora%7"
+console.log(url.replace('%' , '-'))
 
-
-
+console.log(gameName1.split('-'))
 
 
 
