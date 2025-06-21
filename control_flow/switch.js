9753e10e36524cf77b8aebf86ae3a6f8944f807b
Bochan <himanshubora972@gmail.com>
2025-06-21 12:47:49 +0530

    switch case syntax

---
+++ control_flow/switch.js
@@ -0,0 +1,31 @@
+//syntax for switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const monnth =3
+
+switch (monnth) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    
+    default:
+        console.log("ERRRRRRRRORRRRRRRRRRR")
+        break;
+}
