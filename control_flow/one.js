f7d7fa1c8983662bedd92fd0ece7b5df343a58c1
Bochan <himanshubora972@gmail.com>
2025-06-21 12:47:30 +0530

    basic if else synatx in js

---
+++ control_flow/one.js
@@ -0,0 +1,30 @@
+//control flow or logic flow
+
+//if 
+
+// if ( condition ){
+//   //code 
+// }
+
+// === with (strict check) : with equality this checks data type also of both the operands
+
+//variables declared with var datatype can be accessed outside of block scope so it is best to avoid using var datatype instead use let and const
+
+
+// another way to write if conditon without braces
+const balance = 1000
+
+// if(balance > 500 ) console.log("test"),
+// console.log("test 2");
+
+// not a way to write this code 
+
+
+
+
+
+
+
+
+
+
