11b3334a26c01270b9cc688bce92126e17b913fd
himanshubora7 <himanshubora972@gmail.com>
2025-08-17 19:41:48 +0530

    js rev1

--- strings.js
+++ strings.js
@@ -2,33 +2,26 @@
 //string are objets in js
 
 const gameName = new String("hello world");
-console.log(gameName[0])
+console.log(gameName[0]);
 console.log(gameName.__proto__);
 
-console.log(gameName.length)
+console.log(gameName.length);
 
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf("e")+1)
+console.log(gameName.indexOf("e") + 1);
 
-//slicing the string 
-const gameName1 = new String("Bochan-kasukabe")
+//slicing the string
+const gameName1 = new String("Bochan-kasukabe");
 
-const sliced_string = gameName1.substring(0,6);
+const sliced_string = gameName1.substring(0, 6);
 console.log(sliced_string);
 
-const newStringOne = "           bochie     "
+const newStringOne = "           bochie     ";
 
-console.log(newStringOne)
-console.log(newStringOne.trim())
+console.log(newStringOne);
+console.log(newStringOne.trim());
 
-const url ="https://github.com/himanshubora%7"
-console.log(url.replace('%' , '-'))
+const url = "https://github.com/himanshubora%7";
+console.log(url.replace("%", "-"));
 
-console.log(gameName1.split('-'))
-
-
-
-
-
-
-
+console.log(gameName1.split("-"));
